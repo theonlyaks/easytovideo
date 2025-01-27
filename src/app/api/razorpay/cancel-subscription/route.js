c2c1f062b8391c3328c0414a0ca1e70ef3ea2831
developer@input.store <abhishekdeveloper17@gmail.com>
2025-01-28 00:17:33 +0530

    mobile ui added

--- src/app/api/razorpay/cancel-subscription/route.js
+++ src/app/api/razorpay/cancel-subscription/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth/auth" 
 
 export async function POST(request) {
     try {
