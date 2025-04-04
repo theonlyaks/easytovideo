a4462fc958371d3731ee35a2a740b2455cd7b16b
developer@input.store <abhishekdeveloper17@gmail.com>
2025-04-04 21:14:47 +0530

    enhancement

--- src/constants/types/plans.ts
+++ src/constants/types/plans.ts
@@ -74,7 +74,7 @@ export const plansData = [
 
 export const PLAN_TOP_UP:any= {
     'launchOffer': {
-        amount: 100,
+        amount: 500,
         credits: 60,
         name: 'Creator Plan',
         durationInDays: 90,
